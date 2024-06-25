4aa7afe0a65c394b4eab05d16f02bd8064d7d19a
Jasson Rodriguez <ma7rodal@gmail.com>
2024-06-25 14:48:46 -0500

    Add deps

--- mix.exs
+++ mix.exs
@@ -21,6 +21,8 @@ defmodule MovieApp.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:csv, "~> 2.4"},
+      {:ex_doc, "~> 0.25"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
